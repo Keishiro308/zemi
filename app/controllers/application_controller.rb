75c2b8af829a9eba50c5e267d627bef6aaf48356
Keishiro Nishiyama <kn11308@gmail.com>
2020-01-28 15:01:23 +0900

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   private
     def member?
-      unless current_user.member
-        flash[:alert] = '認証されていません'
-        redirect_to member_root_path
+      if current_user.present?
+        unless current_user.member
+          flash[:alert] = '認証されていません'
+          redirect_to member_root_path
+        end
+      else
+        flash[:alert] = 'ログインしてください'
+        redirect_to user_session_path
       end
     end
 end
